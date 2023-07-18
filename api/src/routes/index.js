7f563afce432827fbaad7eaf0a82c0b18e8ae6d9
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-07-18 11:56:55 -0600

    Routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,9 +5,15 @@ const axios = require("axios");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const recipesRouter = require("./recipesRouter");
+const dietsRouter = require("./dietsRouter");
+
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+// configura los enrutadores para las rutas relacionadas con las recetas y las dietas
+
+//REDIRECTING DATA
+router.use("/recipes", recipesRouter);
+router.use("/diets", dietsRouter);
 
 module.exports = router;
