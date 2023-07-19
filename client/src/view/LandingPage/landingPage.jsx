f4904ed91236b6107ed501e227f8164383d04761
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-07-19 17:47:58 -0600

    Package json

--- client/src/view/LandingPage/landingPage.jsx
+++ client/src/view/LandingPage/landingPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import style from './landingPage.module.css';
+import styles from './landingPage.module.css';
 import { Link } from 'react-router-dom';
 
 function LandingPage() {
-    return (
-      <div className={styles.landingPage}>
-        <h1 className={styles.title}>Welcome to Our Food Page!</h1>
-        <Link to="/home">
-          <button>Bon appetite!</button>
-        </Link>
-      </div>
-    );
-  }
-  
-  export default LandingPage;
+  return (
+    <div className={styles.landingPage}>
+      <h1 className={styles.title}>Welcome to Our Food Page!</h1>
+      <Link to="/home">
+        <button>Bon appetite!</button>
+      </Link>
+    </div>
+  );
+}
+
+export default LandingPage;
