ab90ef157815d18e8decfe3552c6cdef4b95d47f
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-07-20 11:01:48 -0600

    NavBar

---
+++ client/src/components/NavBar/navBar.jsx
@@ -0,0 +1,35 @@
+import style from './navBar.module.css'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getRecipesByName } from '../../redux/actions'
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+
+    const dispatch = useDispatch()
+    
+    const [recipeName, setRecipeName] = useState({
+      recipeValue: ''
+    })
+    
+    const handleSearch = (e) =>{
+      setRecipeName({recipeValue: e.target.value})
+    }
+    
+    const onSearch = () =>{
+      dispatch(getRecipesByName(recipeName.recipeValue))
+    }
+    
+      return (
+        
+        <div className={style.divNav}>
+          <Link to='/'> <img className={style.imgNav}/></Link>
+          <input className={style.inputNav} type="text" name='search' value={recipeName.recipeValue} onChange={(e)=>handleSearch(e)} placeholder='recipes..'/>
+          <div>
+            <button className={style.buttonNav} onClick={onSearch}>Search</button>
+          </div>
+        </div>
+      )
+    }
+
+export default NavBar;
